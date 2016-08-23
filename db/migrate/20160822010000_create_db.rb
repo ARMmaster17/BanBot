d0e243623b3460f909f50af0dc01eb31074ed4cb
Joshua Zenn <jzenn@students.kennesaw.edu>
2016-08-23 20:30:41 +0000

    Fixed bad SQL table schema

--- db/migrate/20160822010000_create_db.rb
+++ db/migrate/20160822010000_create_db.rb
@@ -1,5 +1,20 @@
 class CreateDb < ActiveRecord::Migration
   def up
+    drop_table :chats
+    drop_table :badwords
+    create_table :chats do |t|
+  	  t.string :bot_id
+      t.string :group_id
+      t.string :admin_username
+    end
+    create_table :badwords do |t|
+      t.string :group_id
+      t.string :word
+    end
+  end
+  def down
+    drop_table :chats
+    drop_table :badwords
     create_table :chats do |t|
   	  t.integer :bot_id
       t.integer :group_id
@@ -9,16 +24,5 @@ class CreateDb < ActiveRecord::Migration
       t.integer :group_id
       t.string :word
     end
-    create_table :users do |t|
-      t.string :username
-      t.string :hashed_password
-      t.string :followed_groups
-      t.string :user_token
-    end
-  end
-  def down
-    drop_table :chats
-    drop_table :badwords
-    drop_table :users
   end
 end
